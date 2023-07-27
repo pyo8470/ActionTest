f99f7a758dbda22b16e2fd1e6e35177b7c9fbad3
pyo8470 <rkswlska789@naver.com>
2023-07-27 14:21:22 +0900

    First Commit

---
+++ src/main/java/com/example/actiontest/ActiontestApplication.java
@@ -0,0 +1,13 @@
+package com.example.actiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiontestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/actiontest/ActiontestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiontestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
